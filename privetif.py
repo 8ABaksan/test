711ea92e55f723cb968afdab7e632baaa54586d9
MURSHIG <72262848+MURSHIG@users.noreply.github.com>
2020-10-16 18:29:14 +0300

    Добавлен файл с проверкой
    Co-Authored-By: MURSHIG <72262848+MURSHIG@users.noreply.github.com>

---
+++ privetif.py
@@ -0,0 +1,35 @@
+name = input("Введите имя: ")
+if name == "Мурат":
+    print("Привет" + name)
+elif name == "МУРАТ":
+    print("Привет" + name)
+elif name == "мУРАТ":
+    print("Привет" + name)
+elif name == "МуРаТ":
+    print("Привет" + name)
+elif name == "мурат":
+    print("Привет" + name)
+elif name == "МУРАТ":
+    print("Привет" + name)
+elif name == "Эльдар":
+    print("Привет" + name)
+elif name == "ЭЛЬДАР":
+    print("Привет" + name)
+elif name == "ЭлЬдАр":
+    print("Привет" + name)
+elif name == "эльдар":
+    print("Привет" + name)
+elif name == "эЛЬДАР":
+    print("Привет" + name)
+elif name == "Алан":
+    print("Привет" + name)
+elif name == "АЛАН":
+    print("Привет" + name)
+elif name == "АлАн":
+    print("Привет" + name)
+elif name == "алан":
+    print("Привет" + name)
+elif name == "аЛАН":
+    print("Привет" + name)
+else:
+    print("Я тебя не знаю!")
