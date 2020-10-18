52fd235e44523120a09b01c657c9c98bd53fc62a
MURSHIG <72262848+MURSHIG@users.noreply.github.com>
2020-10-18 19:21:01 +0300

    Проверка возраста и роста

---
+++ vozrast_i_rost.py
@@ -0,0 +1,7 @@
+def main (name):
+   vozr = int(input("Введите свой возраст\n"))
+   rost = int(input("Какого ты роста\n?"))
+   name = input ("Как тебя зовут?\n")
+   print("Привет " + name + ", тебе " + vozr + " лет"  + " твой рост: " + rost)
+if __name__ == "__main__":
+    main('PyCharm')
